ab4f549996f950e8abbb22685d65bee6fc8d5d10
Chad Whitacre <chad@zetaweb.com>
2016-01-27 15:04:22 -0500

    Modify db repair to harmonize w/ new condition

--- sql/branch.sql
+++ sql/branch.sql
@@ -1,4 +1,3 @@
 BEGIN;
-    UPDATE payment_instructions SET due = 0 WHERE amount = 0 AND due != 0;
-    UPDATE payment_instructions SET due = floor(9.41/(amount)) * amount WHERE due > 9.41;
+    UPDATE payment_instructions SET due = 0 WHERE due > 9.41;
 END;
